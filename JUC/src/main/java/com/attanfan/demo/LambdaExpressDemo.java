067cea29f8b311e86e0d6c599b574dd8749ea9a0
檀梵 <ll81635957@foxmail.com>
2019-02-20 14:18:03 +0800

    Lambda表达式解析

--- JUC/src/main/java/com/attanfan/demo/LambdaExpressDemo.java
+++ JUC/src/main/java/com/attanfan/demo/LambdaExpressDemo.java
@@ -19,12 +19,26 @@ interface Foo
 }
 
 /**
+ * 使用lambda表达式的前提必须是函数式接口
+ *
+ * 函数式接口：这个类里面只有一个方法
+ *
  * 1    lambda Express
- * ***  拷贝小括号，写死右箭头，落地大括号
+ *
+ *  拷贝小括号，写死右箭头，落地大括号
+ *
  * 1.1 函数式接口才能用Lambda Express
  * 1.2 如何申明一个函数式接口?
+ *  1.2.1:一个接口里有且只有只有一个方法，默认就是函数式接口（隐式声明）
+ *         接口头加@FunctionInterface （显式声明）
+ *
  * 1.3 default支持接口内有方法的实现
- * 1.4
+ *  1.3.1：接口里允许有默认方法的实现
+ *      default开头的 可以有多个
+ *
+ *
+ * 1.4 还可以有静态的方法实现
+ *      使用 类名. 调用
  */
 public class LambdaExpressDemo
 {
@@ -55,5 +69,6 @@ public class LambdaExpressDemo
         System.out.println(foo.add(3, 5));
         System.out.println(foo.div(10, 5));
         System.out.println(Foo.mul(1,5));
+        System.out.println(foo.add(8, 9));
     }
 }
