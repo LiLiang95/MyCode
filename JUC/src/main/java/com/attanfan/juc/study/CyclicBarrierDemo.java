7507abcc4cbb05958287b7e19df389e44f6e4ef1
檀梵 <ll81635957@foxmail.com>
2019-02-20 21:29:41 +0800

    CyclicBarrier循环屏障，先到先等

---
+++ JUC/src/main/java/com/attanfan/juc/study/CyclicBarrierDemo.java
@@ -0,0 +1,27 @@
+package com.attanfan.juc.study;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierDemo {
+
+    public static final int PARTIES = 7;
+
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(PARTIES,()->{System.out.println("召唤神龙");});
+
+        for (int i = 1 ; i <=7 ; i++) {
+            final int tempInt = i;
+             new Thread(()->{
+                 System.out.println(Thread.currentThread().getName()+"\t收集第"+tempInt+"几颗龙珠");
+                 try {
+                     cyclicBarrier.await();
+                 } catch (InterruptedException e) {
+                     e.printStackTrace();
+                 } catch (BrokenBarrierException e) {
+                     e.printStackTrace();
+                 }
+             },String.valueOf(i)).start();
+        }
+    }
+}
